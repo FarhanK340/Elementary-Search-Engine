d762922f9f317f424fd1c118b77264bac9a1dd85
Farhan Kashif <110292152+FarhanK340@users.noreply.github.com>
2023-12-02 12:52:56 +0500

    Searching and Reverse Index Updated

--- reverse_index.py
+++ reverse_index.py
@@ -6,25 +6,41 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from itertools import count
 
-def create_word_index(input_file,output_file):
-    with open(input_file,'r') as f:
-        json_data=json.load(f)
+# def create_word_index(input_file,output_file):
+#     with open(input_file,'r') as f:
+#         json_data=json.load(f)
 
-    word_ids={}
-    for doc_id,stem_words in json_data.items():
-        for stem_word in stem_words:
-            word_id=stem_word
+#     word_ids={}
+#     for doc_id,stem_words in json_data.items():
+#         for stem_word in stem_words:
+#             word_id=stem_word
         
-            if word_id in word_ids:
-              word_ids[word_id].append(int(doc_id))
+#             if word_id in word_ids:
+#               word_ids[word_id].append(int(doc_id))
+#             else:
+#               word_ids[word_id]=[int(doc_id)]
+        
+#     with open(output_file,'w') as f:
+#      json.dump(word_ids,f,indent=2)
+     
+def create_word_index(input_file,output_file):
+    with open(input_file, 'r') as f:
+      json_data = json.load(f)
+      
+      word_id = {}
+      for doc_id, data in json_data.items():
+          for word, frequency in data['word_frequencies'].items():
+            if word in word_id:
+              word_id[word].append({"doc_id": int(doc_id), "frequency": frequency})
             else:
-              word_ids[word_id]=[int(doc_id)]
-        
+              word_id[word] = [{"doc_id": int(doc_id), "frequency": frequency}]
+              
     with open(output_file,'w') as f:
-     json.dump(word_ids,f,indent=2)
+        json.dump(word_id,f,indent=2)
+
         
-input_file='forward_index_2.json'
-output_file='output2.json'
+input_file='forward_index.json'
+output_file='output.json'
 create_word_index(input_file, output_file)
 
 
--- searching.py
+++ searching.py
@@ -2,21 +2,47 @@ import json
 import json
 import string
 import nltk
+import timeit
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from itertools import count
 
-def search_word(file_name,word_from_user):
-       with open(file_name,'r') as f:
+def search_word(forward_index, reverse_index,word_from_user):
+       with open(reverse_index,'r') as f:
            word_ids=json.load(f)
         
        if word_from_user not in word_ids:
             print(f"Opps sorry the '{word_from_user}' not found in the file")
        else:
-            print(f"Doc_ids associated with '{word_from_user}':{word_ids[word_from_user]}" )
-        
-file_name='output2.json'
-word_from_user= input("Enter the word you want to search: ")
-search_word(file_name, word_from_user)
-     
+            # print(f"Doc_ids associated with '{word_from_user}':{word_ids[word_from_user]}" )
+            word_data = word_ids[word_from_user]
+            
+            # Opening the forward index file
+            with open(forward_index,'r') as f_forward:
+                forward_index = json.load(f_forward)
+                
+            # Printing the titles in the forward index of relevant words
+            for data in word_data:
+                doc_id = data['doc_id']
+                if str(doc_id) in forward_index:
+                    # Create a clickable link in HTML format
+                    title = forward_index[str(doc_id)]['title']
+                    url = forward_index[str(doc_id)]['url']
+                    
+                    # clickable_title = f'<a href="{url}" target="_blank">{title}</a>'
+                    clickable_title = f"\033]8;;{url}\033\\{title}\033]8;;\033\\"
+                    print(f"Title associated with '{word_from_user}' in Doc ID {doc_id}: {clickable_title}")
+                    
+forward_index = 'forward_index.json'
+reverse_index = 'output.json'
+# file_name ='output2.json'
+
+word_from_user= input("Enter the word you want to search: ").lower()
+
+# search_word(forward_index, reverse_index, word_from_user)
+
+timer = timeit.Timer(stmt='search_word(forward_index,reverse_index,word_from_user)', globals=globals())
+time_taken_milliseconds = timer.timeit(number=1)
+print(f"Time taken: {time_taken_milliseconds} seconds")
+
