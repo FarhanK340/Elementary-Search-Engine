ddef55c087588d6cebee5f2737afb8e51b899c98
Hadiya-Ali <151432431+Hadiya-Ali@users.noreply.github.com>
2023-12-03 11:07:35 +0500

    adding comments to search and reverse functions

--- reverse_index.py
+++ reverse_index.py
@@ -6,24 +6,10 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from itertools import count
 
-# def create_word_index(input_file,output_file):
-#     with open(input_file,'r') as f:
-#         json_data=json.load(f)
 
-#     word_ids={}
-#     for doc_id,stem_words in json_data.items():
-#         for stem_word in stem_words:
-#             word_id=stem_word
-        
-#             if word_id in word_ids:
-#               word_ids[word_id].append(int(doc_id))
-#             else:
-#               word_ids[word_id]=[int(doc_id)]
-        
-#     with open(output_file,'w') as f:
-#      json.dump(word_ids,f,indent=2)
      
 def create_word_index(input_file,output_file):
+    # opening and reading the file
     with open(input_file, 'r') as f:
       json_data = json.load(f)
       
@@ -36,9 +22,9 @@ def create_word_index(input_file,output_file):
               word_id[word] = [{"doc_id": int(doc_id), "frequency": frequency}]
               
     with open(output_file,'w') as f:
-        json.dump(word_id,f,indent=2)
-
-        
+     json.dump(word_id,f,indent=2)
+      #here we are providing the input_file and also the output_file which can vary with the device that 
+      # you are using.  
 input_file='forward_index.json'
 output_file='output.json'
 create_word_index(input_file, output_file)
--- searching.py
+++ searching.py
@@ -8,14 +8,14 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from itertools import count
 
+
 def search_word(forward_index, reverse_index,word_from_user):
+       #this will just read the file open
        with open(reverse_index,'r') as f:
            word_ids=json.load(f)
-        
        if word_from_user not in word_ids:
             print(f"Opps sorry the '{word_from_user}' not found in the file")
        else:
-            # print(f"Doc_ids associated with '{word_from_user}':{word_ids[word_from_user]}" )
             word_data = word_ids[word_from_user]
             
             # Opening the forward index file
@@ -30,19 +30,17 @@ def search_word(forward_index, reverse_index,word_from_user):
                     title = forward_index[str(doc_id)]['title']
                     url = forward_index[str(doc_id)]['url']
                     
-                    # clickable_title = f'<a href="{url}" target="_blank">{title}</a>'
                     clickable_title = f"\033]8;;{url}\033\\{title}\033]8;;\033\\"
                     print(f"Title associated with '{word_from_user}' in Doc ID {doc_id}: {clickable_title}")
-                    
+# these are the files for forward_index and reverse index        
 forward_index = 'forward_index.json'
 reverse_index = 'output.json'
-# file_name ='output2.json'
 
 word_from_user= input("Enter the word you want to search: ").lower()
 
-# search_word(forward_index, reverse_index, word_from_user)
-
+#finding out the time taken for the word to be searched in seconds
 timer = timeit.Timer(stmt='search_word(forward_index,reverse_index,word_from_user)', globals=globals())
-time_taken_milliseconds = timer.timeit(number=1)
-print(f"Time taken: {time_taken_milliseconds} seconds")
+time_taken_seconds = timer.timeit(number=1)
+print(f"Time taken: {time_taken_seconds} seconds")
+
 
