ba58e685b6b547a0094d166612a27b9113979824
AliHaider1415 <alihaiderur1415@gmail.com>
2023-12-13 19:02:15 +0500

    2nd Updation Reverse-Index Function

--- reverse_index.py
+++ reverse_index.py
@@ -11,18 +11,29 @@ from itertools import count
 def create_word_index(input_file,output_file):
     # opening and reading the file
     with open(input_file, 'r') as f:
-      json_data = json.load(f)
-      
-      word_id = {}
-      for doc_id, data in json_data.items():
-          for word, frequency in data['word_frequencies'].items():
+        json_data = json.load(f)
+
+    word_id = {}
+    for doc_id, data in json_data.items():
+        for word, frequency in data['word_frequencies'].items():
             if word in word_id:
-              word_id[word].append({"doc_id": int(doc_id), "frequency": frequency})
+                word_id[word].append({
+                        "doc_id": int(doc_id),
+                        "frequency": frequency,
+                        "title": data.get("title", ""),  # Include title
+                        "url": data.get("url", "")      # Include url
+                 })
             else:
-              word_id[word] = [{"doc_id": int(doc_id), "frequency": frequency}]
-              
-    with open(output_file,'w') as f:
-     json.dump(word_id,f,indent=2)
+                 word_id[word] = [{
+                        "doc_id": int(doc_id),
+                        "frequency": frequency,
+                        "title": data.get("title", ""),  # Include title
+                        "url": data.get("url", "")      # Include url
+                 }]
+
+# Writing the modified reverse index to the output file
+    with open(output_file, 'w') as f:
+        json.dump(word_id, f, indent=2)
       #here we are providing the input_file and also the output_file which can vary with the device that 
       # you are using.  
 input_file='forward_index.json'
