60049a9727cac2ac6a7a0c74126defd082e13bea
Farhan Kashif <farhankashif6@gmail.com>
2023-12-28 23:09:36 +0500

    removing redundancy from forward index and main

--- forward_index_main.py
+++ forward_index_main.py
@@ -1,5 +1,13 @@
 import os
+import os
 import json
+import string
+import nltk
+import regex
+import unicodedata
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import string
 import nltk
 import regex
@@ -9,8 +17,6 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from itertools import count
 from collections import Counter
-
-
 
 def create_forward_indexing(json_data):
 
@@ -89,31 +95,47 @@ def create_forward_indexing(json_data):
     forward_index = {}
 
     counter_file_path = "counter_id.txt"
+    counter_file_path = "counter_id.txt"
 
+    # Check if the counter file exists
     # Check if the counter file exists
     try:
         with open(counter_file_path, 'r') as counter_file:
             current_counter_value = int(counter_file.read().strip())
     except FileNotFoundError:
         # If the file doesn't exist, create it with the initial value of 1
+        # If the file doesn't exist, create it with the initial value of 1
         current_counter_value = 1
         with open(counter_file_path, 'w') as counter_file:
             counter_file.write(str(current_counter_value))
 
     # Assign the new value to id_counter
+    # Assign the new value to id_counter
     id_counter = count(start=current_counter_value)
 
+    # For Updating some existing file
     # For Updating some existing file
     existing_data = {}
     json_file_path = "alt_forward_index.json"
 
+    # Check if the forward index file exists
     # Check if the forward index file exists
     try:
         with open(json_file_path, 'r') as json_file:
             existing_data = json.load(json_file)
     except FileNotFoundError:
         # If the file doesn't exist, create it with an empty dictionary
+        # If the file doesn't exist, create it with an empty dictionary
         existing_data = {}
+
+
+    # Iterate over articles and tokenize
+    for article in python_data:
+        
+        # Check if the article's URL is already in the forward index
+        existing_entry = next((entry for entry in existing_data.values() if entry["url"] == article["url"]), None)
+        if existing_entry:
+            # print(f"Skipping forward indexing for existing article: {article['url']}")
 
 
     # Iterate over articles and tokenize
@@ -134,15 +156,30 @@ def create_forward_indexing(json_data):
         word_frequencies = Counter(entry["content_tokens"] + list(entry["article_id_tokens"]))
 
         # Assign the next incremental ID to the document
+        entry = tokenize(article)
+
+        # Add title and URL to the entry
+        entry["title"] = article["title"]
+        entry["url"] = article["url"]
+
+        # Count the frequency of each word
+        word_frequencies = Counter(entry["content_tokens"] + list(entry["article_id_tokens"]))
+
+        # Assign the next incremental ID to the document
         doc_id = next(id_counter)
+
+        # Map the title, URL and word frequencies to the document ID in the forward index
 
         # Map the title, URL and word frequencies to the document ID in the forward index
         forward_index[doc_id] = {
             "title": article["title"],
             "url": article["url"],
             "word_frequencies": dict(word_frequencies)
+            "url": article["url"],
+            "word_frequencies": dict(word_frequencies)
         }
 
+    counter_file_path = "counter_id.txt"
     counter_file_path = "counter_id.txt"
     with open(counter_file_path, 'w') as text_file:
         text_file.write(str(next(id_counter)))
@@ -169,7 +206,31 @@ def create_forward_indexing(json_data):
         json.dump(alt_forward_index,json_file, indent = 0)
     return forward_index
         
+    
+    
+    
+    # Updating the forward index
+    alt_forward_index = {}
+
+    alt_forward_index_file = "alt_forward_index.json"
+
+    # Check if the forward index file exists
+    try:
+        with open(alt_forward_index_file, 'r') as json_file:
+            alt_forward_index = json.load(json_file)
+    except FileNotFoundError:
+        # If the file doesn't exist, create it with an empty dictionary
+        alt_forward_index = {}
+
+    new_alt_forward_index = simplify_data(forward_index)
+    alt_forward_index.update(new_alt_forward_index)
+
+    with open(alt_forward_index_file,'w') as json_file:
+        json.dump(alt_forward_index,json_file, indent = 0)
+    return forward_index
+        
 def load_and_format_json(file_path):
+
 
     if not file_path.endswith('.json'):
         print("Error: The file is not a JSON file.")
@@ -183,6 +244,81 @@ def load_and_format_json(file_path):
         print("Error message:", e)
         return None
 
+    formatted_data = json.dumps(json_data, indent=4, ensure_ascii=False)
+    
+    return formatted_data
+  
+def add_forward_index_through_folder(folder_path):
+    all_json_data = []
+    
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                with open(file_path, 'r') as file:
+                    file_data = json.load(file)
+                    all_json_data.extend(file_data)
+            except FileNotFoundError:
+                print("File not found: {file_path}")
+            except json.decoder.JSONDecodeError as e:
+                print(f"Error decoding JSON in file {file_path}: {e}")
+                
+    concatenated_json_data = json.dumps(all_json_data)
+        # this checks for repeating articles if found then it will return none
+    if check_repeating_articles(concatenated_json_data):
+        new_forward_index = create_forward_indexing(concatenated_json_data)
+        return new_forward_index        
+    else:
+        return None
+          
+def add_forward_index_through_single_json_file(file_path):
+
+    r_json_data = load_and_format_json(file_path)
+        
+    if check_repeating_articles(r_json_data):
+        new_forward_index = create_forward_indexing(r_json_data)
+        return new_forward_index
+    else:
+        return None
+    
+def check_repeating_articles(json_data):
+    
+    file_path = "alt_forward_index.json"
+    # Check if the forward index file exists
+    try:
+        with open(file_path, 'r') as json_file:
+            existing_articles = json.load(json_file)
+    except FileNotFoundError:
+    # If the file doesn't exist, create it with an empty dictionary
+        existing_articles = {}
+        
+    # Check if the articles with the same URL already exist in the concatenated data
+    existing_urls = {entry["url"] for entry in existing_articles.values()}
+    if json_data is not None:
+        new_entries = [entry for entry in json.loads(json_data) if entry["url"] not in existing_urls]
+    else:
+        # Handle the case when json_data is None
+        print("Error: JSON data is None.")
+
+    # Skip forward indexing if there are no new entries
+    if not new_entries:
+        print("No new articles to forward index.")
+        return False
+
+    
+        
+    return True
+
+def simplify_data(input_data):
+    simplified_data = {}
+
+    for key, value in input_data.items():
+        simplified_data[key] = {
+            "title": value["title"],
+            "url": value["url"]
+        }
+
+    return simplified_data
     formatted_data = json.dumps(json_data, indent=4, ensure_ascii=False)
     
     return formatted_data
