196172236019f6cfc2cf1653b69f070fb9c6c979
Hadiya-Ali <hadiapapa123@gmail.com>
2023-12-29 10:02:54 +0500

    Resolved Merge Conflicts

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import searching
-import forward_index_alt
 import forward_index_main
 import reverse_index
 import barrel
@@ -25,28 +24,46 @@ def add():
 
     return render_template('addarticle.html')
 
+def create_upload_folder():
+    folder_path = os.path.join(app.root_path, 'uploads')
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
 @app.route('/added', methods=['POST'])
 def added():
-    file_path = request.form['file_path']
-    print(file_path)
-    if file_path:
-        # Execute your Python script with the provided folder path
-        new_forward_index = forward_index_main.add_forward_index_through_single_json_file(file_path)
+    create_upload_folder()
 
-        if new_forward_index is not None:
-            with open('new_forward_index.json', 'w') as nf:
-                json.dump(new_forward_index, nf, indent=0)
+    file_path = request.form.get('file_path')
+    uploaded_file = request.files.get('file_upload')
 
-        forward_index_alt.alt_fi()
+    if not file_path and not uploaded_file:
+        # Handle the case where neither file path nor file upload is provided
+        return render_template('addarticle.html', error='No file provided')
+    #testing
+    if uploaded_file:
+        # Save the uploaded file to a specified path
+        file_path = os.path.join('uploads', uploaded_file.filename)
+        uploaded_file.save(file_path)
 
-        input_file = "forward_index.json"
-        output_file = "reverse_index.json"
+    print("File Path:", file_path)
 
-        if os.path.exists(output_file):
-            input_file = "new_forward_index.json"
+    # Rest of your code
+    new_forward_index = forward_index_main.add_forward_index_through_single_json_file(file_path)
 
-        reverse_index.create_word_index(input_file, output_file)
-        barrel.complete_barrels(output_file)
-    return render_template('addarticle.html')
+    if new_forward_index is not None:
+        with open('new_forward_index.json', 'w') as nf:
+            json.dump(new_forward_index, nf, indent=0)
+
+    input_file = "new_forward_index.json"
+    output_file = "reverse_index.json"
+
+    # if os.path.exists(output_file):
+    #     input_file = "new_forward_index.json"
+
+    reverse_index.create_word_index(input_file, output_file)
+    barrel.complete_barrels(output_file)
+
+    return render_template('addarticle.html', success='File uploaded successfully')
+
 if __name__ == '__main__':
     app.run(debug=True)
